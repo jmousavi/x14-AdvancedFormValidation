bb0ca84d35845d3b1126ca7d6cd17cd91480b40f
jmousavi <jmousavi@mail.csuchico.edu>
2016-10-12 16:30:51 -0700

    initial files with basic validation

---
+++ form-validation.js
@@ -0,0 +1,38 @@
+function validate(){
+  //Grab the user's input and store in variables
+  var userEntered = document.getElementById("user").value;
+  var passEntered = document.getElementById("pass").value;
+
+  //Show message that there is an error with the username...
+  if(userEntered.length < 6){
+  document.getElementById("usernameError").innerHTML="Bad username.";
+  document.getElementById("usernameError").classList.remove("hidden-message");
+  document.getElementById("usernameError").classList.add("shown-message");
+  //Turn the username items red
+  document.getElementById("usernameGroup").classList.add("has-error");
+}
+else{
+  document.getElementById("usernameError").innerHTML="";
+  document.getElementById("usernameError").classList.remove("hidden-message");
+  document.getElementById("usernameGroup").classList.remove("has-error");
+  document.getElementById("usernameError").classList.add("shown-message");
+    document.getElementById("usernameGroup").classList.add("has-success");
+}
+
+
+  //Show message that there is an error with the password...
+  if(passEntered=="password"){
+  document.getElementById("passwordError").innerHTML="Bad password.";
+  document.getElementById("passwordError").classList.remove("hidden-message");
+  document.getElementById("passwordError").classList.add("shown-message");
+  //Turn the password items red
+  document.getElementById("passwordGroup").classList.add("has-error");
+}
+else{
+  document.getElementById("passwordError").innerHTML="";
+  document.getElementById("passwordError").classList.remove("hidden-message");
+  document.getElementById("passwordGroup").classList.remove("has-error");
+  document.getElementById("passwordError").classList.add("shown-message");
+    document.getElementById("passwordGroup").classList.add("has-success");
+}
+}
